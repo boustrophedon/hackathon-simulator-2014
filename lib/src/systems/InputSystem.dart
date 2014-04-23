a52ca69cc5be9ecbde71b0ec126d85fbce02efeb
Harry Stern <hcs@synonymbun.com>
2014-04-23 07:19:27 -0400

    added mouse and touch support to input system

--- lib/src/systems/InputSystem.dart
+++ lib/src/systems/InputSystem.dart
@@ -3,13 +3,41 @@ part of hacksim;
 class InputSystem extends System {
   InputSystem(World world) : super(world) {
     components_wanted = new Set.from([Position,]);
-    // KeyboardEventStream.onKeyDown(document.body).listen(register_keydown);
-    // KeyboardEventStream.onKeyUp(document.body).listen(register_keyup);
     window.onKeyDown.listen(register_keydown);
     window.onKeyUp.listen(register_keyup);
 
+    // replace the window mousedown/move and touchstart/move with canvas
+    // mouseup and touchend should fire regardless of where they end.
+    // mouse/touch are separate so that I can have the MouseEvent and TouchEvent
+    // in the parameters. it is not necessary though; I think I can just use Event or UIEvent.
+    // also, need to actually add the mousemove/touchmove handlers
+    window.onMouseDown.listen(register_mousedown);
+    window.onMouseUp.listen(register_mouseup);
+    window.onTouchStart.listen(register_touchstart);
+    window.onTouchEnd.listen(register_touchend);
+
     world.subscribe_event('KeyDown', handle_keydown);
     world.subscribe_event('KeyUp', handle_keyup);
+
+    world.subscribe_event('MouseDown', handle_mousedown);
+    world.subscribe_event('MouseUp', handle_mouseup);
+    world.subscribe_event('TouchStart', handle_touchstart);
+    world.subscribe_event('TouchEnd', handle_touchend);
+  }
+
+  void register_mousedown(MouseEvent e) {
+    world.send_event('MouseDown', {'MouseEvent':e});
+  }
+  void register_mouseup(MouseEvent e) {
+    world.send_event('MouseUp', {'MouseEvent':e});
+  }
+  void register_touchstart(TouchEvent e) {
+    e.preventDefault();
+    world.send_event('TouchStart', {'TouchEvent':e});
+  }
+  void register_touchend(TouchEvent e) {
+    e.preventDefault();
+    world.send_event('TouchEnd', {'TouchEvent':e});
   }
 
   void register_keydown(KeyEvent e) {
@@ -26,6 +54,26 @@ class InputSystem extends System {
   void handle_keyup(Map event) {
     KeyEvent e = event['KeyEvent'];
     print("KeyUp ${e.keyCode} ${e.charCode}");
+  }
+
+  void handle_mousedown(Map event) {
+    MouseEvent e = event['MouseEvent'];
+    print("MouseDown ${e.client.x} ${e.client.y}");
+  }
+  void handle_mouseup(Map event) {
+    MouseEvent e = event['MouseEvent'];
+    print("MouseUp ${e.client.x} ${e.client.y}");
+  }
+  void handle_touchstart(Map event) {
+    TouchEvent e = event['TouchEvent'];
+    if (e.touches.length > 0) {
+      Touch t = event['TouchEvent'].touches.first;
+      print("TouchStart ${t.client.x} ${t.client.y}");
+    }
+  }
+  void handle_touchend(Map event) {
+    TouchEvent e = event['TouchEvent'];
+    print("TouchEnd");
   }
 
   void process_entity(Entity entity) {}
