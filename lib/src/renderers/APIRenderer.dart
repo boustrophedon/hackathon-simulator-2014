f755c8ef07350e10b2468384d40abe1c6ad81b1e
Harry Stern <hcs@synonymbun.com>
2014-05-28 22:47:54 -0400

    added api name text below apis

--- lib/src/renderers/APIRenderer.dart
+++ lib/src/renderers/APIRenderer.dart
@@ -10,5 +10,12 @@ class APIRenderer extends Renderer {
 
     context.fillStyle = 'rgb(${api.color_string})';
     context.fillRect(pos.x, pos.y, size.width, size.height);
+
+    // this is a hack but whatever
+    // i guess i could create like a uilabel but i didn't really design them to be mobile
+    int xoffset = (context.measureText("${api.name}").width)~/2;
+    int yoffset = 10; // hardcoded and bad
+    context.fillStyle = 'black';
+    context.fillText("${api.name}", pos.x+(size.width~/2)-xoffset, pos.y+size.height+yoffset);
   }
 } 
