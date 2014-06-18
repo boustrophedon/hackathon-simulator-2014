e872bc771795664dce31dfb6f1d683e5107c208e
Harry Stern <hcs@synonymbun.com>
2014-06-18 13:27:59 -0400

    sometimes you lose money when you buy caffeine. I think you maybe should lose hacker cred instead. also probability of losing money needs to be tweaked.

--- lib/src/systems/MoneySystem.dart
+++ lib/src/systems/MoneySystem.dart
@@ -5,8 +5,12 @@ class MoneySystem extends System {
   int api_cost = 50;
   int api_slot_cost = 100;
 
+  math.Random rng;
+
   MoneySystem(World world) : super(world) {
     components_wanted = null;
+
+    rng = new math.Random();
   }
 
   void initialize() {
@@ -14,6 +18,7 @@ class MoneySystem extends System {
 
     world.subscribe_event("BuyAPI", handle_apibuy);
     world.subscribe_event("BuyAPISlot", handle_apislotbuy);
+    world.subscribe_event("GetCaffeine", handle_caffeine);
   }
 
   void handle_apibuy(Map event) {
@@ -26,6 +31,16 @@ class MoneySystem extends System {
     if (world.globaldata['money'] >= api_slot_cost) {
       world.globaldata['money'] -= api_slot_cost;
       world.send_event("SpawnEntity", {'kind':'api slot'});
+    }
+  }
+
+  void handle_caffeine(Map event) {
+    num level = world.globaldata['HackathonsAttended'];
+    if (rng.nextDouble() > (3/level)) {
+      num money = world.globaldata['money'];
+      if (money > 0) {
+        world.globaldata['money'] -= math.min(money, level*5);
+      }
     }
   }
 
